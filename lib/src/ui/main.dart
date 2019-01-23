151ad67fbae62b9d794ce195ba1cff21f46c55ad
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2019-01-23 02:13:11 -0200

    Some styling.

--- lib/src/ui/main.dart
+++ lib/src/ui/main.dart
@@ -43,111 +43,136 @@ class Main extends StatelessWidget {
           ),
         ),
       ),
-      body: SmartRefresher(
-        child: ListView(
-          children: <Widget>[
-            OfflineBuilder(
-              child: Text(
-                "OfflineBuilder child",
-              ),
-              connectivityBuilder: (BuildContext context,
-                  ConnectivityResult connectivity, Widget child) {
-                debugPrint("main connectivity: $connectivity");
-                final bool connected = connectivity != ConnectivityResult.none;
-                debugPrint("main connected: $connected");
-                return connected
-                    ? StreamBuilder(
-                        builder: (BuildContext context,
-                            AsyncSnapshot<List<Offer>> snapshot) {
-                          Widget widget;
-                          debugPrint(
-                              "main snapshot.connectionState: ${snapshot.connectionState}");
-                          switch (snapshot.connectionState) {
-                            case ConnectionState.active:
-                              debugPrint(
-                                  "main snapshot.hasError: ${snapshot.hasError}");
-                              debugPrint(
-                                  "main snapshot.hasData: ${snapshot.hasData}");
-                              if (snapshot.hasError) {
-                                widget = Center(
-                                  child: Column(
-                                    children: <Widget>[
-                                      SizedBox(
-                                        height: 10.0,
-                                      ),
-                                      Text(
-                                        "$errorText: ${snapshot.error}",
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              } else if ((!snapshot.hasData) ||
-                                  (snapshot.data.length == 0)) {
-                                widget = Center(
-                                  child: Column(
-                                    children: <Widget>[
-                                      SizedBox(
-                                        height: 10.0,
-                                      ),
-                                      Text(
-                                        noOffersText,
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              } else {
-                                widget = ListView.builder(
-                                  shrinkWrap: true,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return ListTile(
-                                      title: Text(
-                                        snapshot.data[index].title,
-                                      ),
-                                      subtitle: Text(
-                                        snapshot.data[index].description,
-                                      ),
-                                      trailing: Text(
-                                        "R\$ ${currencyNumberFormat.format(snapshot.data[index].price)}",
-                                      ),
-                                    );
-                                  },
-                                  itemCount: snapshot.data.length,
-                                );
-                              }
-                              widget = Center(
-                                child: widget,
-                              );
-                              break;
-                            default:
-                              widget = Center(
-                                child: Column(
-                                  children: <Widget>[
-                                    SizedBox(
-                                      height: 10.0,
+      body: Container(
+        child: SmartRefresher(
+          child: ListView(
+            children: <Widget>[
+              OfflineBuilder(
+                child: Text(
+                  "OfflineBuilder child",
+                ),
+                connectivityBuilder: (BuildContext context,
+                    ConnectivityResult connectivity, Widget child) {
+                  debugPrint("main connectivity: $connectivity");
+                  final bool connected =
+                      connectivity != ConnectivityResult.none;
+                  debugPrint("main connected: $connected");
+                  return connected
+                      ? StreamBuilder(
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<Offer>> snapshot) {
+                            Widget widget;
+                            debugPrint(
+                                "main snapshot.connectionState: ${snapshot.connectionState}");
+                            switch (snapshot.connectionState) {
+                              case ConnectionState.active:
+                                debugPrint(
+                                    "main snapshot.hasError: ${snapshot.hasError}");
+                                debugPrint(
+                                    "main snapshot.hasData: ${snapshot.hasData}");
+                                if (snapshot.hasError) {
+                                  widget = Center(
+                                    child: Column(
+                                      children: <Widget>[
+                                        SizedBox(
+                                          height: 10.0,
+                                        ),
+                                        Text(
+                                          "$errorText: ${snapshot.error}",
+                                        ),
+                                      ],
                                     ),
-                                    Loader(),
-                                  ],
-                                ),
-                              );
-                              break;
-                          }
-                          return widget;
-                        },
-                        stream: bloc.offersStream,
-                      )
-                    : Text(
-                        youAreNotConnectedText,
-                      );
-              },
-            ),
-          ],
+                                  );
+                                } else if ((!snapshot.hasData) ||
+                                    (snapshot.data.length == 0)) {
+                                  widget = Center(
+                                    child: Column(
+                                      children: <Widget>[
+                                        SizedBox(
+                                          height: 10.0,
+                                        ),
+                                        Text(
+                                          noOffersText,
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                } else {
+                                  snapshot.data.sort((offer1, offer2) =>
+                                      offer1.title.compareTo(offer2.title));
+                                  widget = ListView.builder(
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return Container(
+                                        child: ListTile(
+                                          title: Text(
+                                            snapshot.data[index].title,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                            snapshot.data[index].description,
+                                          ),
+                                          trailing: Text(
+                                            "R\$ ${currencyNumberFormat.format(snapshot.data[index].price)}",
+                                            style: TextStyle(
+                                              color: Colors.red,
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(
+                                              5.0,
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    itemCount: snapshot.data.length,
+                                    shrinkWrap: true,
+                                  );
+                                }
+                                widget = Center(
+                                  child: widget,
+                                );
+                                break;
+                              default:
+                                widget = Center(
+                                  child: Column(
+                                    children: <Widget>[
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Loader(),
+                                    ],
+                                  ),
+                                );
+                                break;
+                            }
+                            return widget;
+                          },
+                          stream: bloc.offersStream,
+                        )
+                      : Text(
+                          youAreNotConnectedText,
+                        );
+                },
+              ),
+            ],
+          ),
+          controller: _refreshController,
+          onRefresh: (bool up) async {
+            await bloc.loadOffers();
+            _refreshController.sendBack(up, RefreshStatus.completed);
+          },
         ),
-        controller: _refreshController,
-        onRefresh: (bool up) async {
-          await bloc.loadOffers();
-          _refreshController.sendBack(up, RefreshStatus.completed);
-        },
+        padding: const EdgeInsets.all(
+          10.0,
+        ),
       ),
     );
   }
